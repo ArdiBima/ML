86d4fba25acf9944a4f51c88123169c3b597988f
ICEMATERWELON <ardi.bputra@gmail.com>
2023-05-30 20:14:25 +0700

    Add deploy code python

---
+++ Deploy.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+# Load the model architecture from JSON
+model_architecture_path = 'image_classifier_model_3.json'
+with open(model_architecture_path, 'r') as json_file:
+    loaded_model_json = json_file.read()
+
+# Load the model weights
+model_weights_path = 'image_classifier_weights.h5'
+
+# Load the image to make predictions on
+image_path = 'image.jpg'
+image = tf.keras.preprocessing.image.load_img(image_path, target_size=(150, 150))
+input_data = tf.keras.preprocessing.image.img_to_array(image)
+input_data = np.expand_dims(input_data, axis=0)
+input_data /= 255.0
+
+# Load the model
+loaded_model = tf.keras.models.model_from_json(loaded_model_json)
+loaded_model.load_weights(model_weights_path)
+
+# Make predictions using the loaded model
+predictions = loaded_model.predict(input_data)
+predicted_class = np.argmax(predictions[0])
+
+# Define the class labels alphabetically
+class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']  
+
+# Print the predicted class label
+print('Predicted class:', class_labels[predicted_class])
